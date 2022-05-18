64ba5ce16a61140bb65a7b17cf4cef59665b027c
Sammy Nyakabau <sammy@sammynyakabau.com>
2022-05-19 03:43:01 +0530

    more bug fixes

--- src/components/CollegeList.svelte
+++ src/components/CollegeList.svelte
@@ -23,7 +23,12 @@
       title: "Colleges",
       value: v => {
         const name = v.colleges
-        const src = $collegesMap?.name['collegeExpressUrl'] || "#"
+        const collegeObject = $collegesMap[name]
+
+        if(!collegeObject){
+          return name
+        }
+        const src = collegeObject['collegeExpressUrl'] || "#"
        return `<a href='${src}' target="_blank">${name}</a>`
       },
       sortable: true,
