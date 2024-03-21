609d2f461373b0542b986a265328fd7a5a6b7e44
Sammy Nyakabau <sammy@sammynyakabau.com>
2024-03-21 22:16:58 +0200

    fixes everything
    Signed-off-by: Sammy Nyakabau <sammy@sammynyakabau.com>

--- src/types/airtable.ts
+++
@@ -1,3 +0,0 @@
-import type {Attachment, Collaborator} from "airtable";
-
-export type AirtableField = string | number | boolean | Collaborator | readonly Collaborator[] | readonly string[] | readonly Attachment[] | undefined;
